a28c745b41b8263304dd6605e553d78fca692da7
akram6t <khanakram8435@gmail.com>
2022-12-08 00:24:20 +0530

    something

---
+++ questions/verbal.js
@@ -0,0 +1,96 @@
+
+    let antonyms=[
+        {
+            question:'ENORMOUS',
+            op1:'Soft',
+            op2:'Average',
+            op3:'Tiny',
+            op4:'Weak',
+            exp:'',
+        },
+        {
+            question:'ARTIFICIAL',
+            op1:'Red',
+            op2:'Natural',
+            op3:'Truthful',
+            op4:'Solid',
+            //exp:'',
+        },
+        {
+            question:'EXPAND',
+            op1:'Convert',
+            op2:'Condense',
+            op3:'Congest',
+            op4:'Conclude',
+            //exp:'',
+        },
+        {
+            question:'MORTAL',
+            op1:'Divine',
+            op2:'Immortal',
+            op3:'Spiritual',
+            op4:'Eternal',
+            //exp:'',
+        },
+        {
+            Questio5:'OBEYING',
+            op1:'Ordering',
+            op2:'Following',
+            op3:'Refusing',
+            op4:'Contradicting',
+            //exp:'',
+        },
+        {
+            question6:'BUSY',
+            op1:'Occupied',
+            op2:'Engrossed',
+            op3:'Relaxed',
+            op4:'Engaged',
+            //exp:'',
+        }
+       
+    ]
+
+
+    
+    let syntonyms=[
+        {
+            question:'BRIEF',
+            op1:'Limited',
+            op2:'Small',
+            op3:'Little',
+            op4:'Short',
+        },
+        {
+            question:'AUGUST',
+            op1:'Common',
+            op2:'Ridiculous',
+            op3:'Dignified',
+            op4:'Petty',
+        },
+        {
+            Question:'ALERT',
+            op1:'Energetic',
+            op2:'Observant',
+            op3:'Observant',
+            op4:'Watchful',
+        },
+        {
+            Question:'WARRIOR',
+            op1:'Soldier',
+            op2:'Sailor',
+            op3:'Pirate',
+            op4:'Spy',
+
+        
+        },
+        {
+            Question:'FAKE',
+            op1:'Original',
+            op2:'Imitation',
+            op3:'Trustworthy',
+            op4:'Loyal',
+        }
+    ]
+
+
